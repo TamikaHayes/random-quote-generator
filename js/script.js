ac07126baefd99522e7fea4f852860d441186b40
TamikaHayes <tamika.hayes@gmail.com>
2021-03-08 23:09:08 -0800

    Added quotes array to script.js
    An array of quote objects, all with sources, and some with citations and years

--- js/script.js
+++ js/script.js
@@ -10,7 +10,32 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: 'We choose to go to the Moon in this decade and do the other things, not because they are easy, but because they are hard.',
+    source: 'John F. Kennedy',
+    citation: 'Address at Rice University on the Nation Space Effort',
+    year: 1962
+  },
+  {
+    quote: 'We are already life-chosen by ourselves. We are the moral inhabitants of the galaxy.',
+    source: 'Toni Morrison',
+    citation: 'Sarah Lawrence College Commencement Address',
+    year: 1988
+  },
+  {
+    quote: 'Above all, be the heroine of your life, not the victim.',
+    source: 'Nora Ephron'
+  },
+  {
+    quote: 'What you do makes a difference, and you have to decide what kind of difference you want to make.',
+    source: 'Jane Goodall'
+  },
+  {
+    quote: 'The way I see it, if you want the rainbow, you gotta put up with the rain!',
+    source: 'Dolly Parton'
+  }
+];
 
 
 /***
